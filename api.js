c7ae3517b34603b229db22f9333cb976ed4d6444
Arman7052 <99arman7052@gmail.com>
2023-03-09 00:43:57 +0600

    Loading spinner added .

--- api.js
+++ api.js
@@ -150,4 +150,12 @@ const showAllCard = () => {
     })
 };
 
+window.addEventListener("load", () => {
+  document.querySelector(".loader").classList.add("loader--hidden");
+
+  document.querySelector(".loader").addEventListener("transitionend", () => {
+    document.body.removeChild(document.querySelector(".loader"));
+  })
+});
+
 loadAllData();
